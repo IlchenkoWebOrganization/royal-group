b6952cb297095b0eacf300753d886cc15c8bdc27
IlchenkoDev7 <sashailchenko7@yandex.ru>
2024-07-25 00:58:33 +0300

    slider now is driggable

--- src/scripts/main-scripts/AdaptiveSliderForThreeCards.js
+++ src/scripts/main-scripts/AdaptiveSliderForThreeCards.js
@@ -6,6 +6,9 @@ export default class AdaptiveSliderForThreeCards {
         this.prevButton = document.querySelector(prevButtonSelector);
         this.nextButton = document.querySelector(nextButtonSelector);
         this.currentSlide = 0;
+        this.startX = 0;
+        this.currentX = 0;
+        this.isDragging = false;
 
         this.updateSlidesToShow();
         this.updateSliderPosition();
@@ -13,6 +16,9 @@ export default class AdaptiveSliderForThreeCards {
         this.prevButton.addEventListener('click', this.showPrevSlide.bind(this));
         this.nextButton.addEventListener('click', this.showNextSlide.bind(this));
         window.addEventListener('resize', this.onResize.bind(this));
+        this.sliderTrack.addEventListener('touchstart', this.onTouchStart.bind(this));
+        this.sliderTrack.addEventListener('touchmove', this.onTouchMove.bind(this));
+        this.sliderTrack.addEventListener('touchend', this.onTouchEnd.bind(this));
     }
 
     updateSlidesToShow() {
@@ -48,5 +54,36 @@ export default class AdaptiveSliderForThreeCards {
     onResize() {
         this.updateSlidesToShow();
         this.updateSliderPosition();
+    }
+
+    onTouchStart(event) {
+        this.startX = event.touches[0].clientX;
+        this.isDragging = true;
+        this.sliderTrack.style.transition = 'none'; // Disable transitions during drag
+    }
+
+    onTouchMove(event) {
+        if (!this.isDragging) return;
+        this.currentX = event.touches[0].clientX;
+        const deltaX = this.currentX - this.startX;
+        const slideWidth = this.slides[0].clientWidth;
+        const newTranslateX = -this.currentSlide * slideWidth + deltaX;
+        this.sliderTrack.style.transform = `translateX(${newTranslateX}px)`;
+    }
+
+    onTouchEnd() {
+        this.isDragging = false;
+        const slideWidth = this.slides[0].clientWidth;
+        const movedBy = this.currentX - this.startX;
+
+        if (movedBy > slideWidth / 3 && this.currentSlide > 0) {
+            this.showPrevSlide();
+        } else if (movedBy < -slideWidth / 3 && this.currentSlide < this.slides.length - this.slidesToShow) {
+            this.showNextSlide();
+        } else {
+            this.updateSliderPosition();
+        }
+
+        this.sliderTrack.style.transition = ''; // Re-enable transitions
     }
 }
